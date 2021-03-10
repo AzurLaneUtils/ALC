365954b59c0ac487153a6f9c1d966fd8c9112ca0
Xymul <3143718176@qq.com>
2021-03-10 23:01:53 +0800

    add src

---
+++ alc-api/src/main/kotlin/org/alu/AzurLaneCalculator/core/warships/LightAircraftCarrier.kt
@@ -0,0 +1,36 @@
+package com.xymul.AzurLaneCalculator.core.warships
+
+import com.xymul.AzurLaneCalculator.core.Type
+import com.xymul.AzurLaneCalculator.core.Warship
+import com.xymul.AzurLaneCalculator.core.helper.EquipmentSLots
+import com.xymul.AzurLaneCalculator.core.helper.WarshipWeapons
+
+/**
+ * 表示一艘轻航
+ * */
+class LightAircraftCarrier(
+    override val affection: Warship.Affection,
+    override val level: Int,
+    override val name: String,
+    override val health: Int,
+    override val armorType: Type.ArmorType,
+    override val reload: Int,
+    override val lucky: Int,
+    override val evasion: Int,
+    override val speed: Int,
+    override val antiAircraft: Int,
+    override val aviation: Int,
+    override val cost: Int,
+    override val accuracy: Int,
+    override val antiSubmarine: Int,
+    override val weaponSlots: WarshipWeapons,
+    override val equipmentSLots: EquipmentSLots,
+) : Warship {
+    override val shipType: Type.ShipType = Type.ShipType.CVL
+    override val hasMainGun: Boolean = false
+    override val hasTorpedo: Boolean = false
+    override val hasSecondaryGun: Boolean = false
+    override val hasAntiAircraftGun: Boolean = true
+    override val firepower: Int = 0
+    override val torpedo: Int = 0
+}
